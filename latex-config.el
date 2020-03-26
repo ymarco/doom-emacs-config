9523c80dbca39a6c91c106d393dafca79a7cf87e
yoavm448 <yoavm448@gmail.com>
2020-03-26 13:15:39 +0200

    Move the cdlatex config back into latex-config
    It's not big enough to demand its own file anymore

--- latex-config.el
+++ latex-config.el
@@ -1,8 +1,6 @@
 ;;; ~/.config/doom/latex-config.el -*- lexical-binding: t; -*-
 ;; Notice that this contains no math config, that's on modules/lang/math
 
-(load! "cdlatex-config")
-
 ;;; Misc config
 (defvar +latex-use-TeX-fold t
   "Use TeX fold in TeX-mode.
@@ -149,8 +147,21 @@ typically insert macros."
 ;;; Keybinds
 
 (add-hook! 'TeX-mode-hook
-           ;; complete is 100% annoying me here, just adds lag
+           ;;(hl-todo-mode) ; TODO
+           ;; Bigger compiled math cause it's pretty
+           (setq-default preview-scale 1.8)
+           (setq preview-scale 1.8)
+           ;; Completion is 100% annoying me here, adds tons of lag and gives
+           ;; terrible suggestions
+           ;; TODO Debug those company backends, maybe fix completion with texlab
            (company-mode -1))
+
+;; mixed-pitch is pretty
+(add-hook 'TeX-mode-hook #'mixed-pitch-mode)
+;; WIP mine and @itai33 plugin
+(add-hook 'TeX-mode-hook #'evil-tex-mode)
+
+;;; Keybinds
 
 (after! tex
   (map!
@@ -165,3 +176,23 @@ typically insert macros."
      :desc "Unfold paragraph"   "C-f" #'TeX-fold-clearout-paragraph
      :desc "Fold buffer"        "F"   #'TeX-fold-buffer
      :desc "Unfold buffer"      "C-F" #'TeX-fold-clearout-buffer)))
+(setq
+ cdlatex-math-symbol-prefix ?\;
+ cdlatex-math-symbol-alist
+ '( ;; adding missing functions to 3rd level symbols
+   ;;key   1st            2nd           3rd
+   (?_    ("\\downarrow"  ""           "\\inf"))
+   (?^    ("\\uparrow"    ""           "\\sup"))
+   (?k    ("\\kappa"      ""           "\\ker"))
+   (?m    ("\\mu"         ""           "\\lim"))
+   (?d    ("\\delta"      "\\partial"  "\\dim"))
+   (?D    ("\\Delta"      "\\nabla"    "\\deg"))
+   ;; no idea why \Phi isnt on 'F' in first place, \phi is on 'f'.
+   (?F    ("\\Phi"))
+   ;; now just conveniance
+   (?:    ("\\dots")))
+ cdlatex-math-modify-alist
+ '( ;; my own stuff
+   ;;key  mathcmd   textcmd  type  rmdot(?)  it(?)
+   (?/    "\\oner"  nil      t     nil       nil)
+   (?h    "\\half"  nil      t     nil       nil)))

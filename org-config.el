bc688fdf068c4560b47e5cca980a431a446b424c
Yoav Marco <yoavm448@gmail.com>
2020-08-24 15:55:48 +0300

    Remove custom org latex highlighting
    The Doom default knows what its doing

--- org-config.el
+++ org-config.el
@@ -42,10 +42,6 @@
 
 (setq org-ellipsis " ▾")
 
-;; Native LaTeX highlighting is slower but WAY prettier
-(after! org
-  (setq org-highlight-latex-and-related '(latex script entities)))
-
 ;; Ultra-bold annoys me, and bold is annoying when theres too much of it.
 ;; Semi-bold is a lot prettier (at least when supported).
 (custom-set-faces!
